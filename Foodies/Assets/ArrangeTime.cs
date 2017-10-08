84baaf50455fa3bc6f3e8a236cc1ce0bc471b758
Zhanggeyao <moscato6226790@gmail.com>
2017-10-07 20:16:58 -0400

    ClickToPopUp
    Geyao

--- Foodies/Assets/ArrangeTime.cs
+++ Foodies/Assets/ArrangeTime.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class ArrangeTime : MonoBehaviour {
 
+	public int pan;
+	//public int knifes
+	public string[] busyActions;
+	public string[] relaxActions;
 	// Use this for initialization
 	void Start () {
-		
-	}
+		busyActions = new string[]{ "stri", "fly", " cut" };
+		relaxActions = new string[]{"boil","bake"};
+
+        //recipes = GameObject.FindGameObjectsWithTag("Recipe");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // ThisStep = recipe.GetComponent<Text>().text;
+        //if()
+    }
 }
---
+++ Foodies/Assets/ClickToPopup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToPopup : MonoBehaviour {
+
+    public GameObject PopupPrefab;
+    private float space;
+    // Use this for initialization
+    public void PopUp() {
+        space = 120.0f;
+        GameObject PopupMessage = Instantiate(PopupPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        PopupMessage.transform.SetParent(transform.parent);
+        PopupMessage.transform.localPosition = transform.localPosition+transform.right*space;
+    }
+}
